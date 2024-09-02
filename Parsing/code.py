19db8e52bf11a73286d52bee7c2f36dfebb29448
SlavaChepelin <slavachepelin@mail.ru>
2024-09-02 22:59:00 +0300

    new idea

--- Parsing/code.py
+++ Parsing/code.py
@@ -1,62 +1,13 @@
-from __future__ import print_function
-import os.path
-import pickle
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from bs4 import BeautifulSoup
+import csv
+import requests
 
-SAMPLE_RANGE_NAME = 'Test List!A2:E246'
-
-
-class GoogleSheet:
-    SPREADSHEET_ID = 'YOUR_SPREADSHEET_ID'
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    service = None
-
-    def __init__(self):
-        creds = None
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                print('flow')
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-
-        self.service = build('sheets', 'v4', credentials=creds)
-
-    def updateRangeValues(self, range, values):
-        data = [{
-            'range': range,
-            'values': values
-        }]
-        body = {
-            'valueInputOption': 'USER_ENTERED',
-            'data': data
-        }
-        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
-                                                                  body=body).execute()
-        print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
-
-
-def main():
-    gs = GoogleSheet()
-    test_range = 'Test List!G2:H4'
-    test_values = [
-        [16, 26],
-        [36, 46],
-        [56, 66]
-    ]
-    gs.updateRangeValues(test_range, test_values)
-
-
-if __name__ == '__main__':
-    main()
+html = requests.get('https://docs.google.com/spreadsheets/d/1dy_i_s5Cn4DEZnl87_j5YdkAL4Yf1Gq9RTe_wVwoHzk/pubhtml').text
+soup = BeautifulSoup(html, "lxml")
+tables = soup.find_all("table")
+index = 0
+for table in tables:
+    with open(str(index) + ".csv", "w") as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
+    index = index + 1   
