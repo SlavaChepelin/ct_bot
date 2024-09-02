c857b9b0c6122f423b6e1e8d291ac94e9d708d4d
SlavaChepelin <slavachepelin@mail.ru>
2024-09-03 00:07:54 +0300

    Добавлен парсер

--- Parsing/code.py
+++ Parsing/code.py
@@ -5,9 +5,12 @@ import requests
 html = requests.get('https://docs.google.com/spreadsheets/d/1dy_i_s5Cn4DEZnl87_j5YdkAL4Yf1Gq9RTe_wVwoHzk/pubhtml').text
 soup = BeautifulSoup(html, "lxml")
 tables = soup.find_all("table")
-index = 0
-for table in tables:
-    with open(str(index) + ".csv", "w") as f:
-        wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-        wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
-    index = index + 1   
+
+
+with open("ans.csv", "w") as f:
+    wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    wr.writerows([[td.text for td in row.find_all("td")] for row in tables[0].find_all("tr")])
+with open('ans.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
