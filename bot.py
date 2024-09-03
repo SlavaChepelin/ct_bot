d61e28d9ba940bf96324885255c51a1faa300494
Sagiman <sasha@sagiman.ru>
2024-09-03 11:33:22 +0300

    Добавление bot.py
    В bot.py находится основной скрипт программы, через который подтягиваются обрабатывающие модули

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from handlers import admin, basic_commands
+
+
+load_dotenv()
+token: str = os.getenv("TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    bot = Bot(token=token)
+    dp = Dispatcher()
+
+    dp.include_routers(basic_commands.basic_router,
+                       admin.admin_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
