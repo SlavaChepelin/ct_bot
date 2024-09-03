12ef9d3ba233cc42dccdc29e012b2cfca1f460db
Sagiman <sasha@sagiman.ru>
2024-09-03 11:34:58 +0300

    Обновление db_schedule_changer.py
    В db_schedule_changer.py добавлена новая функция, способная к извлечению данных об изменениях в БД по запрошенной дате

--- scripts/db_schedule_changer.py
+++ scripts/db_schedule_changer.py
@@ -10,10 +10,11 @@ async def add_schedule_change(group: str,
 
     db = await aiosqlite.connect('''data_bases/schedule_changes.db''')
 
+    # Создаем таблицу "Changes", если она еще не была создана
     await db.execute("""
     CREATE TABLE IF NOT EXISTS Changes (
     id INTEGER PRIMARY KEY,
-    group TEXT NOT NULL, 
+    "group" TEXT NOT NULL, 
     date TEXT NOT NULL,
     time TEXT NOT NULL,
     subject TEXT NOT NULL,
@@ -22,9 +23,24 @@ async def add_schedule_change(group: str,
     )
     """)
 
-    await db.execute("INSERT INTO Changes (group, date, time, subject, lesson_type, auditorium) "
+    # Добавляем в БД новую запись об изменениях
+    await db.execute("INSERT INTO Changes ('group', date, time, subject, lesson_type, auditorium) "
                      "VALUES (?, ?, ?, ?, ?, ?)",
-                     group, date, time, subject, lesson_type, auditorium)
+                     (group, date, time, subject, lesson_type, auditorium))
 
     await db.commit()
     await db.close()
+
+
+async def get_schedule_changes(date: str):
+    db = await aiosqlite.connect('''data_bases/schedule_changes.db''')
+
+    # Ищем в БД все совпадения по дате и выгружаем в "result"
+    cursor = await db.execute(f"SELECT 'group', date, time, subject, lesson_type, auditorium FROM Users "
+                              f"WHERE (date == {date})")
+    result = await cursor.fetchall()
+
+    await db.commit()
+    await db.close()
+
+    return result
