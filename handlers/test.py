8bb486c096ab3dbf121c2c52faeed4f75a361a68
SlavaChepelin <slavachepelin@mail.ru>
2024-09-08 12:22:27 +0300

    Update test.py

--- handlers/test.py
+++ handlers/test.py
@@ -13,15 +13,15 @@ test_router.message.filter()
 
 @test_router.message(Command("dbplus"))
 async def dbplus(message: Message):
-    await db_schedule.add_schedule_plus(39, "2024.09.03", 2, "Матан", "пр", "2444", "Кохась" )
+    await db_schedule.add_schedule_plus(139, "2024.09.03", 2, "Матан", "пр", "2444", "Кохась" )
     
 @test_router.message(Command("dbminus"))
 async def dbminus(message: Message):
-    await db_schedule.add_schedule_minus(39, "2024.09.03", 3)
+    await db_schedule.add_schedule_minus(139, "2024.09.03", 3)
 
 @test_router.message(Command("dballadd"))
 async def dballadd(message: Message):
-    await db_all.add_all(39,1,1,"матан","пр","2333","Кохась")
+    await db_all.add_all(139,1,1,"матан","пр","2333","Кохась")
 @test_router.message(Command("dballget"))
 async def dballget(message: Message):
     s = await db_all.get_row(39,1,1)
@@ -33,29 +33,29 @@ async def dbupdater(message: Message):
 
 @test_router.message(Command("gettable"))
 async def gettable(message: Message):
-   print(await updating.get_schedule(39,"2024.09.03"))
+   print(await updating.get_schedule(139,"2024.09.03"))
 
 @test_router.message(Command("getanswer"))
 async def getanswer(message: Message):
-   print(await updating.beatiful_schedule(39,"2024.09.03"))
+   print(await updating.beatiful_schedule(139,"2024.09.03"))
 
 @test_router.message(Command("addminus"))
 async def addminus(message: Message):
-   await formatting.cancel_lessons(39, "2024.09.03",5)
+   await formatting.cancel_lessons(139, "2024.09.03",5)
 
 @test_router.message(Command("addplus"))
 async def addplus(message: Message):
-   await formatting.add_lessons(39, "2024.09.03",5,"1","1","1","1")
+   await formatting.add_lessons(139, "2024.09.03",5,"1","1","1","1")
 
 @test_router.message(Command("transfer_lessons"))
 async def transfer_lessons(message: Message):
-   await formatting.transfer_lessons(39, "2024.09.04",4, "2024.09.04",5)
+   await formatting.transfer_lessons(139, "2024.09.04",4, "2024.09.04",5)
    
 @test_router.message(Command("delete_changes"))
 async def delete_changes(message: Message):
-   await formatting.delete_changes(39, "2024.09.04")
+   await formatting.delete_changes(139, "2024.09.04")
 
 @test_router.message(Command("get_schedule"))
 async def get_schedule(message: Message):
-   print(await formatting.get_schedule(39, "2024.09.04"))
+   print(await formatting.get_schedule(139, "2024.09.04"))
 
